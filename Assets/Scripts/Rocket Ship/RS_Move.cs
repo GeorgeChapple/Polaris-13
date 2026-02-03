668a0dc69ea31fc372484a849d07345ce0ca89d5
JUK3 <146436532+JTRMreal@users.noreply.github.com>
2026-02-03 15:51:49 +0000

    ship movement script structure outlined

--- Assets/Scripts/Rocket Ship/RS_Move.cs
+++ Assets/Scripts/Rocket Ship/RS_Move.cs
@@ -1,43 +1,93 @@
 using UnityEngine;
 
 // Made by: George Chapple, Jason Lodge
+// Summary: we're not actually moving the ship, we're going to be moving all of the stuff inside the parent
 
 public class RS_Move : MonoBehaviour
 {
-    public Vector3 position;
-    public Vector3 direction;
+    [Header("References")]
+    public GameObject globalParent;
+
+    [Header("Global Values")]
+    public Vector3 worldPosition;
+    public Vector3 worldDirection;
     public float speed;
 
-    public enum moveMode { Manual, Automatic }
+    public enum moveMode { Manual, Automatic, Deactivated }
 
     public moveMode mode = moveMode.Manual;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Settings")]
+    [SerializeField] private float speedChangeAmt = 5;
+    [SerializeField] private float directionChangeSpeedMax = 5;
+    [SerializeField] private float directionChangeSpeedMin = 5;
 
-    // Update is called once per frame
+    [SerializeField] private Vector3 anchorPoint = Vector3.zero;
+
+    private Vector2 controllerDir = Vector2.zero; // max of 1 on both axis positive and negative
+    private float targetSpeed;
+
     void Update()
     {
         if (mode == moveMode.Manual)
         {
-            MoveShip(position, direction); //actually should only need direction
+            // Update Direction
+            // Update Movement
+            UpdateDirection();
+            MoveShip();
+        }
+        else if (mode == moveMode.Automatic)
+        {
+            AutoMoveShip(worldPosition);
         }
         else
         {
-            MoveShip(position);
+            // when ship is deactivated, do nothing
         }
     }
 
-    void MoveShip(Vector3 posToMoveTo)
+    void AutoMoveShip(Vector3 posToMoveTo)
     {
-        position = Vector3.Lerp(position, direction * speed, Time.deltaTime);
+        // lerp ship pos to
+        worldPosition = Vector3.Lerp(worldPosition, worldDirection * speed, Time.deltaTime);
+
+        // lerp ship rotation to
+        
     }
 
-    void MoveShip(Vector3 posToMoveTo, Vector3 direction)
+    void MoveShip()
     {
-        position = Vector3.Lerp(position, direction * speed, Time.deltaTime);
+        // lerp to target speed and clamp to target speed when close enough
+
+
+        // move ship in direction using speed value
+
+        
+    }
+
+    void UpdateDirection()
+    {
+        if (mode == moveMode.Manual)
+        {
+            // change world direction based on distance of controller v2 from zero
+
+            // calculate percentage of vec2 controller from zero to max
+            // add to world direction using calculated speed value with max and min clamp
+        }
+    }
+
+    void ChangeSpeed(bool upOrDown, float amount, bool emergencyStop)
+    {
+        if (emergencyStop) { targetSpeed = 0; return; }
+
+        if (upOrDown) // up
+        {
+            targetSpeed += amount;
+        }
+        else // down
+        {
+            targetSpeed -= amount;
+        }
+        // intervals of 5 i think
     }
 }
