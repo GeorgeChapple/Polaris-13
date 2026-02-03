134a0fd3df384a7145e9503df967f52fe1299016
JUK3 <146436532+JTRMreal@users.noreply.github.com>
2026-02-03 14:21:28 +0000

    little start on ship movement
    will have manual and auto
    auto only needs positions
    manual needs direction, and speed

--- Assets/Scripts/Rocket Ship/RS_Move.cs
+++ Assets/Scripts/Rocket Ship/RS_Move.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 
+// Made by: George Chapple, Jason Lodge
+
 public class RS_Move : MonoBehaviour
 {
     public Vector3 position;
     public Vector3 direction;
     public float speed;
+
+    public enum moveMode { Manual, Automatic }
+
+    public moveMode mode = moveMode.Manual;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +20,23 @@ public class RS_Move : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (mode == moveMode.Manual)
+        {
+            MoveShip(position, direction); //actually should only need direction
+        }
+        else
+        {
+            MoveShip(position);
+        }
+    }
+
+    void MoveShip(Vector3 posToMoveTo)
+    {
+        position = Vector3.Lerp(position, direction * speed, Time.deltaTime);
+    }
+
+    void MoveShip(Vector3 posToMoveTo, Vector3 direction)
     {
         position = Vector3.Lerp(position, direction * speed, Time.deltaTime);
     }
