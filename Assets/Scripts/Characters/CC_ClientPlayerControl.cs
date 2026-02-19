2b17609dcd17dd52ff7630eb19866455d7976c60
George <147159990+GeorgeChapple@users.noreply.github.com>
2026-02-19 16:31:54 +0000

    Camera fix test 4

--- Assets/Scripts/Characters/CC_ClientPlayerControl.cs
+++ Assets/Scripts/Characters/CC_ClientPlayerControl.cs
@@ -59,7 +59,7 @@ public class CC_ClientPlayerControl : NetworkBehaviour
         m_PlayerInput.enabled = false;
         m_PlayerInputManager.enabled = false;
         m_CharacterPlayerController.enabled = false;
-        cam.SetActive(false);
+        Destroy(cam);
 
         StopAllCoroutines();
 
