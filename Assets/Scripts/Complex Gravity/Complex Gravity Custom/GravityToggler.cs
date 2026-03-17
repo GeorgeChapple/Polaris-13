6f70b331afff32f4ce19ab90c12faebc085ec60e
ArranBrocklehurst <s4308679@glos.ac.uk>
2026-03-17 14:42:48 +0000

    Fixed gravity issues with point of interest
    This is Jimbo, not Arran, dont get it twisted.

--- Assets/Scripts/Complex Gravity/Complex Gravity Custom/GravityToggler.cs
+++ Assets/Scripts/Complex Gravity/Complex Gravity Custom/GravityToggler.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class GravityToggler : MonoBehaviour
 {
     [SerializeField] private bool useGravity;
+    [SerializeField] private bool planetoid;
 
     private void OnTriggerEnter(Collider col)
     {
@@ -20,6 +21,10 @@ public class GravityToggler : MonoBehaviour
         if (gravityBody != null)
         {
             gravityBody.triggered = false;
+            if (planetoid)
+            {
+                gravityBody.useGravity = !useGravity;
+            }
         }
     }
 }
