7b3e22a071a7dbfba214a25febdcc0617cb1dbea
JUK3 <146436532+JTRMreal@users.noreply.github.com>
2026-04-13 17:09:07 +0100

    start of shop
    figurin it out

---
+++ Assets/Scripts/Shop/SHP_ShopProfiles.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+// Made By: Jason Lodge.
+// Summary: This class is the shop profiles class, it will handle all the shop profiles and the shop's inventory along side price margins.
+
+public class SHP_ShopProfiles : ScriptableObject
+{
+
+}
