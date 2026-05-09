3b7cb46ce647aebc6264a15296c6f3731b10029f
George <147159990+GeorgeChapple@users.noreply.github.com>
2026-05-09 17:09:00 +0100

    Started Shader for Space Map
    *Generates Texture2D for cluster positions.
    *VFX graph samples texture2d colours to create particle positions.

--- Assets/Scripts/Space/SP_MapGenerator.cs
+++ Assets/Scripts/Space/SP_MapGenerator.cs
@@ -1,21 +1,21 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.VFX;
 
 public class SP_MapGenerator : MonoBehaviour
 {
     public Vector3 mapSize;
     public Biome[] biomes;
     public int seed;
+    public List<ClusterManager> clusters = new List<ClusterManager>();
+    [SerializeField] private GameObject TEMP;
+    public Texture2D positionData;
+    private int spawned = 0;
+    private VisualEffect effect;
 
-    public List<Cluster> clusters = new List<Cluster>();
 
-    private void Awake()
-    {
-        UnityEngine.Random.InitState(seed);
-        clusters = GenerateMap(biomes[0]);
-    }
 
     [Serializable]
     public struct Biome
@@ -31,6 +31,20 @@ public class SP_MapGenerator : MonoBehaviour
         public int maxSpawn;
     }
 
+    [Serializable]
+    public struct ClusterManager
+    {
+        public bool activated;
+        public List<Cluster> clusterGroup;
+
+        public ClusterManager(List<Cluster> _clusters)
+        {
+            activated = false;
+            clusterGroup = _clusters;
+        }
+    }
+
+    [Serializable]
     public struct Cluster
     {
         public GameObject prefab;
@@ -47,13 +61,51 @@ public class SP_MapGenerator : MonoBehaviour
         }
     }
 
-    private List<Cluster> GenerateMap(Biome biome)
+    private void Awake()
     {
-        List<Cluster> newClusters = new List<Cluster>();
+        effect = GetComponent<VisualEffect>();
+        UnityEngine.Random.InitState(seed);
+        clusters = GenerateMap(biomes[0]);
+    }
+
+    private void FixedUpdate()
+    { 
+        Texture2D newData = new Texture2D(spawned, 1, TextureFormat.RGBA32_SIGNED, false);
+        int i = 0;
+        foreach (ClusterManager manager in clusters)
+        {
+            foreach(Cluster cluster in manager.clusterGroup)
+            {
+                newData.SetPixel(i, 1, PositionToColour(cluster.position, mapSize));
+                i++;
+            }
+        }
+        newData.Apply();
+        positionData = newData;
+        effect.SetTexture("_positionData", positionData);
+        effect.SetInt("_spawnCount", spawned);
+    }
+
+    private Color PositionToColour(Vector3 position, Vector3 bounds) 
+    {
+        Color newPosition = new Color(
+            position.x / bounds.x,
+            position.y / bounds.y,
+            position.z / bounds.z,
+            1f
+            );
+        return newPosition;
+    }
+
+    private List<ClusterManager> GenerateMap(Biome biome)
+    {
+        spawned = 0;
+        List<ClusterManager> newClusterManagers = new List<ClusterManager>();
         foreach (ClusterSpawn spawn in biome.clusterSpawns)
         {
             for (int i = 0; i < UnityEngine.Random.Range(spawn.minSpawn, spawn.maxSpawn); i++)
             {
+                List<Cluster> newClusters = new List<Cluster>();
                 SP_Cluster prefab = spawn.clusterPrefab;
                 Vector3 newPosition = Vector3.zero;
                 float spaceRadius = UnityEngine.Random.Range(prefab.spaceRadius.x, prefab.spaceRadius.y);
@@ -98,26 +150,32 @@ public class SP_MapGenerator : MonoBehaviour
                     offset *= UnityEngine.Random.Range(prefab.spawnRadius.x, prefab.spawnRadius.y);
 
                     Cluster newCluster = new Cluster(
-                        prefab.prefab,
+                        prefab.prefabs[UnityEngine.Random.Range(0, prefab.prefabs.Length)],
                         newPosition + offset,
                         UnityEngine.Random.Range(prefab.triggerRadius.x, prefab.triggerRadius.y),
                         prefab.colour
                         );
                     newClusters.Add(newCluster);
+                    spawned++;
                 }
+                ClusterManager newManager = new ClusterManager(newClusters);
+                newClusterManagers.Add(newManager);
             }
         }
-        return newClusters;
+        return newClusterManagers;
     }
 
     private void OnDrawGizmos()
     {
         CustomGizmos.DrawBox(Vector3.zero, transform.rotation, mapSize, Color.yellow);
-        foreach (Cluster cluster in clusters)
+        foreach (ClusterManager manager in clusters)
         {
-            Color gizmoColour = cluster.colour * new Color(1, 1, 1, 0.5f);
-            Gizmos.color = gizmoColour;
-            Gizmos.DrawSphere(cluster.position, cluster.radius);
+            foreach (Cluster cluster in manager.clusterGroup)
+            {
+                Color gizmoColour = cluster.colour * new Color(1, 1, 1, 0.5f);
+                Gizmos.color = gizmoColour;
+                Gizmos.DrawSphere(cluster.position, cluster.radius);
+            }
         }
     }
 }
